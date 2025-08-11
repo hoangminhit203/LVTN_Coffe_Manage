c0e5f79e2ab475dba425bcd4610e700d27f0ce74
hoangminh203 <hoangminhit203@gmail.com>
2025-08-12 01:20:26 +0700

    Updae Core
    Firist-Step-Core

---
+++ BE_Coffe_Manage/BE_Coffe_Manage/Coffe_Manage.Infrastructure.EF/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿//using Coffee_Manage.Domain;
+//using System.Threading.Tasks;
+
+//namespace Coffee_Manage.Infrastructure
+//{
+//    public class UnitOfWork : IUnitOfWork
+//    {
+//        private readonly DbContext _context;
+
+//        public UnitOfWork(DbContext context)
+//        {
+//            _context = context;
+//        }
+
+//        public async Task<int> SaveChangesAsync()
+//        {
+//            return await _context.SaveChangesAsync();
+//        }
+
+//        public void Dispose()
+//        {
+//            _context.Dispose();
+//        }
+//    }
+//}
